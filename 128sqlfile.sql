7c3b26915f86bc0018e628d9086095e1cf782684
Amaanish <135824090+Amaanish@users.noreply.github.com>
2025-05-24 14:19:39 +0400

    Add files via upload

---
+++ 128sqlfile.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE recipe_db;
+
+USE recipe_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+
+select * from users;
